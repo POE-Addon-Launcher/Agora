76d0f43e831e3ebb24e5c4c8641f5c368a15155a
Rizlim <Rizlim@user.noreply.github.com>
2019-12-22 12:59:39 +0100

    Configure Kotlin

---
+++ Addons/src/main/kotlin/Launcher.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+/**
+ *
+ */
+class Launcher
+{
+    companion object
+    {
+        @JvmStatic
+        fun main(args: Array<String>)
+        {
+            // Set Locale to US to avoid any locale issues.
+            Locale.setDefault(Locale.US)
+        }
+    }
+}
